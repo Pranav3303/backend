ff942fe85c807b56378c0309a89596d75037734e
Pranav3303 <160731119+Pranav3303@users.noreply.github.com>
2025-12-19 12:24:11 +0530

    1

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    # path('info/',views.info),
-    # path('register/',views.tv_reg),
-    # path('emp/<str:pk>',views.indiv),
-    # path('update/<str:pk>',views.update),
-    # path('delete/<str:pk>',views.delete),
+    path('info/',views.info),
+    path('register/',views.tv_reg),
+    path('emp/<str:pk>',views.indiv),
+    path('update/<str:pk>',views.update),
+    path('delete/<str:pk>',views.delete),
     path('tvs/', views.tv_list),
 ]
